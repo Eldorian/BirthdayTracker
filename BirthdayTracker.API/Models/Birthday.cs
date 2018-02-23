c786062e06b5f053a6f3415af7237f7773b14e90
James R Cook <jer_cook79@hotmail.com>
2018-02-23 11:49:19 -0600

    Adding back in API

---
+++ BirthdayTracker.API/Models/Birthday.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BirthdayTracker.API.Models
+{
+    public class Birthday
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Relationship { get; set; }
+    }
+}
---
+++ BirthdayTracker.API/Startup.cs
@@ -0,0 +1,48 @@
+﻿using BirthdayTracker.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace BirthdayTracker.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<BirthdayContext>(options => options.UseSqlite("Data Source=Birthday.db"));
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new Info {Title = "Birthday Tracker", Version = "v1"})
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Birthday Tracker v1")
+            );
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            BirthdayContext.SeedData(app.ApplicationServices);
+        }
+    }
+}
